57a5a0c643d5490420884ca63f2708332e9fbe49
RahulScripted <goswami.rahul1002@gmail.com>
2024-09-18 06:45:18 +0530

    Add files via upload

---
+++ Learn-CPP-Codechef/Algorithmic Problems/CustomInput.cpp
@@ -0,0 +1,17 @@
+// Replace the Custom inputs with your own created inputs and click Run to check the result.
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        int N;
+        cin >> N;
+        cout << N + 1 << endl;
+    }
+}
---
+++ Learn-CPP-Codechef/Arrays/CreatingArrays.cpp
@@ -0,0 +1,12 @@
+// Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num[] = {1, 2, 3, 4 ,5 };
+    cout << "Done";
+}
